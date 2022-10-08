b18919ecd7dd0cf023d383a08a7773e98cf58313
laggart <laggart@gmail.com>
2022-10-09 01:35:08 +0200

    first commit

---
+++ src/components/RegistreForm.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="signInForm fixed-center" style="max-width: 700px">
+    <div class="q-pa-md my-font">
+      <div class="q-gutter-md row items-start">
+        <q-input
+          clearable
+          clear-icon="close"
+          filled
+          class="text-h5 q-my-md"
+          v-model="text"
+          label="Name"
+        />
+        <q-input
+          clearable
+          clear-icon="close"
+          filled
+          class="text-h5 q-my-md"
+          v-model="text"
+          label="Last Name"
+        />
+      </div>
+      <div class="q-gutter-md row items-start">
+        <q-input 
+          clearable
+          clear-icon="close"
+          filled
+          class="q-my-md"
+          v-model="email" 
+          type="email" hint="Please insert a valid mail"
+          label="E-mail" 
+        />
+        <q-input
+          clearable
+          clear-icon="close"
+          filled
+          class="text-h5 q-my-md"
+          v-model="text"
+          label="User Name"   
+        />
+        <q-input v-model="password" 
+        filled 
+        label="Password"
+        :type="isPwd ? 'password' : 'text'" 
+        hint="Please insert a valid password">
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+      </div>
+     
+      
+      <div class="q-pa-md q-gutter-sm">
+        <q-btn color="primary" class="full-width" label="Submit" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      password: ref(''),
+      isPwd: ref(true),
+      email: ref('')
+    }
+  }
+}
+</script>
+<style>
+.signInForm {
+  background-color: rgb(195, 216, 216);
+  border-radius: 20px;
+}
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-layout view="hHh lpR fFf" class="bg-dark">
+
+    <q-header reveal elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title class="my-font text-h1">
+          <q-avatar>
+          <p class=" text-h6 text-weight-bold"> D&D</p>  
+          </q-avatar>
+          Iron D&D
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="justify my-font">
+        <q-route-tab to="/page1" label="Demo" />
+        <q-route-tab to="/SignIn" label="Create an account" />
+        <q-route-tab to="/page3" label="Log In" />
+      </q-tabs>
+    </q-header>
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div class="my-font">Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+
+</script>
