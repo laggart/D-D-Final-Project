fb629212d53e539087224286115aa22aa703188b
laggart <laggart@gmail.com>
2022-10-29 19:19:38 +0200

    fixing img to deploy again

--- src/components/CharPage.vue
+++ src/components/CharPage.vue
@@ -285,7 +285,7 @@ export default defineComponent({
     const $q = useQuasar();
     const router = useRouter();
     const miniState = ref(false);
-    const url = ref("../src/assets/placeholder.png");
+    const url = ref("./../src/assets/placeholder.png");
     const userStore = useUserStore();
     const charStore = useCharStore();
     const { user } = storeToRefs(userStore);
